8594177a2fcaf8f845f8d721366882cf7fefae56
铁兰 <tielan@fengyudeMacBook-Air.local>
2017-11-01 21:35:31 +0800

    修改日数据变化

--- app/containers/analysis/home/S_DayPage.js
+++ app/containers/analysis/home/S_DayPage.js
@@ -30,21 +30,26 @@ export default class S_DayPage extends React.Component {
       data: [],
       day: day,
       selectItem: undefined,
-      selectCust:true,
+      selectCust: true,
+      loading: false,
     }
   }
   componentDidMount() {
     this._selectByDate(this.state.day);
   }
   _selectByDate(day) {
+    this.setState({ day, loading: true })
     InteractionManager.runAfterInteractions(() => {
       FetchManger.getUri('dataCenter/appHomePage/getDayFactory.page', { day }).then((responseData) => {
         if (responseData.status === '0' || responseData.status === 0) {
           let data = responseData.data.dayList;
-          this.setState({ data })
+          this.setState({ data, loading: false })
+        } else {
+          this.setState({ loading: false })
         }
-      }).catch((error) => {
 
+      }).catch((error) => {
+        this.setState({ loading: false })
       })
     });
   }
@@ -54,7 +59,7 @@ export default class S_DayPage extends React.Component {
   }
 
   swithItemPress() {
-    this.setState({selectCust :!this.state.selectCust});
+    this.setState({ selectCust: !this.state.selectCust });
   }
   render() {
     let listData = this.state.data;
@@ -92,7 +97,7 @@ export default class S_DayPage extends React.Component {
           </TouchableOpacity>
           <DatePicker
             style={{ width: 100, }}
-            date={this.state.deliverydate}
+            date={this.state.day}
             customStyles={{
               dateInput: { borderWidth: 0 },
               dateText: { fontSize: 18, color: '#000', textAlign: 'left' }
@@ -114,73 +119,71 @@ export default class S_DayPage extends React.Component {
           </TouchableOpacity>
           <View style={{ flex: 1 }} />
         </View>
-        <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#fff' }}>
-          <View style={{ width: 100, justifyContent: 'center', alignItems: 'center' }}>
-            <LeftTabComponet
-              data={listData}
-              sectionAction={(item) => {
-                this.setState({ selectItem: item })
-              }}
-            />
-          </View>
-          <ScrollView style={{ flex: 1, backgroundColor: '#f9f9f9', }}>
-            <View style={{ flex: 1, backgroundColor: '#f9f9f9', flexDirection: 'column' }}>
-              <View style={{ backgroundColor: '#fff', marginTop: 12, marginRight: 12, marginLeft: 12, flexDirection: 'row' }}>
-                <TouchableOpacity style={{ flex: 1, flexDirection: 'row' }} onPress={this.onItemUpAction}>
-                  <View style={{ borderWidth: 1, borderColor: '#61aee0', flex: 1, backgroundColor: '#61aee0', borderRadius: 4, flexDirection: 'row' }}>
-                    <Text style={{ fontSize: 12, padding: 8, color: '#fff' }}>{`销售总额${selectItem.totalSum ? selectItem.totalSum : 0}万元`}</Text>
-                  </View>
-                </TouchableOpacity>
-                <View style={{ width: 12, backgroundColor: '#f9f9f9', }} />
-                <TouchableOpacity style={{ flex: 1, flexDirection: 'row' }} onPress={this.onItemAction}>
-                  <View style={{ borderWidth: 1, borderColor: '#61aee0', flex: 1, backgroundColor: '#61aee0', borderRadius: 4, flexDirection: 'row' }}>
-                    <Text style={{ fontSize: 12, padding: 8, color: '#fff' }}>{`未收 ${selectItem.unReceiverSum ? selectItem.unReceiverSum : 0}万元`}</Text>
-                  </View>
-                </TouchableOpacity>
+        {
+          this.state.loading ? <LoadingView /> :
+            <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#fff' }}>
+              <View style={{ width: 100, justifyContent: 'center', alignItems: 'center' }}>
+                <LeftTabComponet
+                  data={listData}
+                  sectionAction={(item) => {
+                    this.setState({ selectItem: item })
+                  }}
+                />
               </View>
-              <View style={{ backgroundColor: '#fff', margin: 12, padding: 12 }}>
-                <Text style={{ color: '#666', fontSize: 12 }}>{'当日销售冠军'}</Text>
-                <Text style={{ height: 32, color: '#17c6c1', marginTop: 4, marginBottom: 10 }}>{dayMax}</Text>
-                <Text style={{ color: '#666', fontSize: 12 }}>{'当日最大客户'}</Text>
-                <Text style={{ height: 32, color: '#17c6c1', marginTop: 4, marginBottom: 8 }}>{dayCustomer}</Text>
-              </View>
-              <Text style={{ color: '#999', marginLeft: 12, marginTop: 12, fontSize: 12 }}>{'销售额前20排名情况'}</Text>
-              <View style={{ backgroundColor: '#fff', margin: 10 }}>
-                <View style={{ height: 38, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center' }}>
-                  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', height: 38 }}>
-                    <TouchableOpacity style={{ flex: 1, height: 38, alignItems: 'center', justifyContent: 'center' }} onPress={this.swithItemPress.bind(this, 0)}>
-                      <Text style={{ color: !this.state.selectCust ? '#0081d4' : '#222' }}>{'客户'}</Text>
+              <ScrollView style={{ flex: 1, backgroundColor: '#f9f9f9', }}>
+                <View style={{ flex: 1, backgroundColor: '#f9f9f9', flexDirection: 'column' }}>
+                  <View style={{ backgroundColor: '#fff', marginTop: 12, marginRight: 12, marginLeft: 12, flexDirection: 'row' }}>
+                    <TouchableOpacity style={{ flex: 1, flexDirection: 'row' }} onPress={this.onItemUpAction}>
+                      <View style={{ borderWidth: 1, borderColor: '#61aee0', flex: 1, backgroundColor: '#61aee0', borderRadius: 4, flexDirection: 'row' }}>
+                        <Text style={{ fontSize: 12, padding: 8, color: '#fff' }}>{`销售总额${selectItem.totalSum ? selectItem.totalSum : 0}万元`}</Text>
+                      </View>
                     </TouchableOpacity>
-                    <View style={{ height: 1, backgroundColor: !this.state.selectCust ? '#0081d4' : '#c4c4c4', width: (WINDOW_WIDTH - 120) / 2 }} />
+                    <View style={{ width: 12, backgroundColor: '#f9f9f9', }} />
+                    <TouchableOpacity style={{ flex: 1, flexDirection: 'row' }} onPress={this.onItemAction}>
+                      <View style={{ borderWidth: 1, borderColor: '#61aee0', flex: 1, backgroundColor: '#61aee0', borderRadius: 4, flexDirection: 'row' }}>
+                        <Text style={{ fontSize: 12, padding: 8, color: '#fff' }}>{`未收 ${selectItem.unReceiverSum ? selectItem.unReceiverSum : 0}万元`}</Text>
+                      </View>
+                    </TouchableOpacity>
                   </View>
-                  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', height: 38 }}>
-                    <TouchableOpacity style={{ flex: 1, height: 38, alignItems: 'center', justifyContent: 'center' }} onPress={this.swithItemPress.bind(this, 1)}>
-                      <Text style={{ color: this.state.selectCust ? '#0081d4' : '#222' }}>{'业务员'}</Text>
-                    </TouchableOpacity>
-                    <View style={{ height: 1, backgroundColor: this.state.selectCust ? '#0081d4' : '#c4c4c4', width: (WINDOW_WIDTH - 120) / 2 }} />
+                  <View style={{ backgroundColor: '#fff', margin: 12, padding: 12 }}>
+                    <Text style={{ color: '#666', fontSize: 12 }}>{'当日销售冠军'}</Text>
+                    <Text style={{ height: 32, color: '#17c6c1', marginTop: 4, marginBottom: 10 }}>{dayMax}</Text>
+                    <Text style={{ color: '#666', fontSize: 12 }}>{'当日最大客户'}</Text>
+                    <Text style={{ height: 32, color: '#17c6c1', marginTop: 4, marginBottom: 8 }}>{dayCustomer}</Text>
+                  </View>
+                  <Text style={{ color: '#999', marginLeft: 12, marginTop: 12, fontSize: 12 }}>{'销售额前20排名情况'}</Text>
+                  <View style={{ backgroundColor: '#fff', margin: 10 }}>
+                    <View style={{ height: 38, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center' }}>
+                      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', height: 38 }}>
+                        <TouchableOpacity style={{ flex: 1, height: 38, alignItems: 'center', justifyContent: 'center' }} onPress={this.swithItemPress.bind(this, 0)}>
+                          <Text style={{ color: !this.state.selectCust ? '#0081d4' : '#222' }}>{'客户'}</Text>
+                        </TouchableOpacity>
+                        <View style={{ height: 1, backgroundColor: !this.state.selectCust ? '#0081d4' : '#c4c4c4', width: (WINDOW_WIDTH - 120) / 2 }} />
+                      </View>
+                      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', height: 38 }}>
+                        <TouchableOpacity style={{ flex: 1, height: 38, alignItems: 'center', justifyContent: 'center' }} onPress={this.swithItemPress.bind(this, 1)}>
+                          <Text style={{ color: this.state.selectCust ? '#0081d4' : '#222' }}>{'业务员'}</Text>
+                        </TouchableOpacity>
+                        <View style={{ height: 1, backgroundColor: this.state.selectCust ? '#0081d4' : '#c4c4c4', width: (WINDOW_WIDTH - 120) / 2 }} />
+                      </View>
+                    </View>
+                    {
+                      this.state.selectCust ?
+                        employeeList.map((item, index) => <RowRightView key={`index_${index}`} item={item} />)
+                        :                      
+                        customerList.map((item, index) => <RowLeftView key={`index_${index}`} item={item} />)
+                    }
                   </View>
                 </View>
-                {
-                  customerList.map((item, index) => {
-                    let empName = '';
-                    let empSalerSum = '';
-                    if (employeeList && employeeList.length > index) {
-                      empName = employeeList[index].empName;
-                      empSalerSum = employeeList[index].empSalerSum;
-                    }
-                    return <RowView key={`index_${index}`} item={item} />
-                  })
-                }
-              </View>
-            </View>
-          </ScrollView>
-        </View >
+              </ScrollView>
+            </View >
+        }
       </View >
     );
   }
 }
 
-class RowView extends React.Component {
+class RowLeftView extends React.Component {
   render() {
     let onPress = this.props.onPress
     let item = this.props.item;
@@ -193,8 +196,29 @@ class RowView extends React.Component {
           <Text style={{ color: '#999', fontSize: 12, flex: 1, textAlign: 'right' }}>{item.cusPhone}</Text>
         </View>
         <View style={{ flexDirection: 'row' }}>
-          <Text style={{ color: '#f80000', fontSize: 12,marginTop: 5,flex: 1 }}>{`销售 ${item.customerSalerSum}元`}</Text>
-          <Text style={{ color: '#f80000', fontSize: 12, marginTop: 5,flex: 1, textAlign: 'right' }}>{`未收'${item.unpaidSum}元`}</Text>
+          <Text style={{ color: '#f80000', fontSize: 12, marginTop: 5, flex: 1 }}>{`销售 ${item.customerSalerSum}元`}</Text>
+          <Text style={{ color: '#f80000', fontSize: 12, marginTop: 5, flex: 1, textAlign: 'right' }}>{`未收'${item.unpaidSum}元`}</Text>
+        </View>
+      </View>
+      <View style={{ height: StyleSheet.hairlineWidth, backgroundColor: '#d9d9d9' }} />
+    </TouchableOpacity>
+  }
+}
+
+class RowRightView extends React.Component {
+  render() {
+    let onPress = this.props.onPress
+    let item = this.props.item;
+    return <TouchableOpacity onPress={() => {
+      onPress && onPress()
+    }}>
+      <View style={{ backgroundColor: '#fff', padding: 16 }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ color: '#333', flex: 1 }}>{item.empName}</Text>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ color: '#f80000', fontSize: 12, marginTop: 5, flex: 1 }}>{`销售 ${item.empSalerSum}元`}</Text>
+          <Text style={{ color: '#f80000', fontSize: 12, marginTop: 5, flex: 1, textAlign: 'right' }}>{`未收'${item.unpaidSum}元`}</Text>
         </View>
       </View>
       <View style={{ height: StyleSheet.hairlineWidth, backgroundColor: '#d9d9d9' }} />
