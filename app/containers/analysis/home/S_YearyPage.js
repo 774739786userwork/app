ddcb978b027581b9fdca620cc9508cc75aa38a1e
chensilin <774739786@qq.com>
2018-01-28 18:07:46 +0800

    bbb

--- app/containers/analysis/home/S_YearyPage.js
+++ app/containers/analysis/home/S_YearyPage.js
@@ -267,10 +267,10 @@ export default class S_YearyPage extends React.Component {
                                             <Text style={{ width: 68, color: '#17c6c1' }}>{`${item.factoryTotalSum}`}</Text>
                                         </TouchableOpacity>
                                         <Text style={{ color: '#666' }}>{'退'}</Text>
-                                        <Text style={{ width: 68, color: '#17c6c1' }}>{`${item.returnTotalSum}万`}</Text>
+                                        <Text style={{ width: 68, color: '#17c6c1' }}>{`${item.returnTotalSum}`}</Text>
                                         <Text style={{ color: '#666' }}>{'未'}</Text>
                                         <TouchableOpacity onPress={this.onNuShowAction.bind(this, item)}>
-                                            <Text style={{ width: 68, color: '#f80000' }}>{`${item.factoryUnReceiveSum}万`}</Text>
+                                            <Text style={{ width: 68, color: '#f80000' }}>{`${item.factoryUnReceiveSum}`}</Text>
                                         </TouchableOpacity>
                                     </View>
                                     <View style={{ height: StyleSheet.hairlineWidth, backgroundColor: '#dedede' }}></View>
